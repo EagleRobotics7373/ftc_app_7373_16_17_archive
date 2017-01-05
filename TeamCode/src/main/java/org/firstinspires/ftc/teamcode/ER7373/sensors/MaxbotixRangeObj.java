c1b2acc30210e6e026994b6beea6e05e30385d59
JordanMoss <kk4jrq@gmail.com>
2017-01-04 21:31:14 -0500

    created test and obj for the maxbotix range sensor

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ER7373/sensors/MaxbotixRangeObj.java
@@ -0,0 +1,50 @@
+package org.firstinspires.ftc.teamcode.ER7373.sensors;
+
+/**
+ * Created by JordanMoss on 1/4/17.
+ *
+ * This class is for the Maxbotix Range finder using an analog connection on the CDIM
+ *
+ */
+import com.qualcomm.robotcore.hardware.AnalogInput;
+
+
+public class MaxbotixRangeObj {
+    //paramaters
+    AnalogInput range;
+    double raw;
+    double calculated;
+
+    //values to approximate distance based on voltage
+    public static final double[] corVoltage ={};
+    public static final double[] corDistance = {};
+
+
+    /**
+     * constructor for the range sensor
+     *
+     * 1 arg for the sensor
+     *
+     * created using hardware map get for analogInput device
+     */
+
+    public MaxbotixRangeObj(AnalogInput r){
+        range = r;
+
+    }
+
+    //method to read the raw analog value
+    public double rawRead(){
+        return range.getVoltage();
+    }
+
+    //method to read the raw data and convert to distance
+    public double distance(){
+        raw = range.getVoltage();
+
+        //code to convert
+
+
+        return calculated;
+    }
+}
